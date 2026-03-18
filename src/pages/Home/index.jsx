f3462a84a5e43996a5237ece6850a57ff723693c
atomicmaker <383518694@qq.com>
2026-03-18 13:32:20 +0800

    first transform

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,43 @@
 import './index.css'
+import LinkCard from '../../components/LinkCard'
+import ProfileFrame from '../../components/ProfileFrame'
+import SideBar from '../../components/SideBar'
+import TimeCard from '../../components/TimeCard'
+import ProfilePicture from '../../assets/img/profile.jpg'
 
 const Home = () => {
     return (
         <div className="main">
-            <p>这是我的博客</p>
+            <div className='leftside'>
+                <div className="card-container selection">
+                    <div className="introduction">
+                        <img src={ ProfilePicture } style={{width:"60px", borderRadius:"50px", margin:"0 15px 0 0"}}></img>
+                        <p>不会替身(开发中)</p>
+                    </div>
+                    <SideBar></SideBar>
+                </div>
+            </div>
+
+            <div className='midside' style={{marginTop:"10%"}}>
+                <div className='card-container'>
+
+                </div>
+
+                <div className='card-container'>
+                    <div><ProfileFrame></ProfileFrame></div>
+                    <p style={{fontSize:"20px"}}>A simple soul with a passion for</p>
+
+                </div>
+
+                <div style={{marginTop:"10%"}}>
+                    <LinkCard></LinkCard>
+                </div>
+            </div>
+
+            <div className='rightside'>
+                <div></div>
+                <TimeCard></TimeCard>
+            </div>
         </div>
     )
 }
